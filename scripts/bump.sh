da6c08799707a388feb2e4f53672869c0ded06b5
Viktor Mörsell <viktor@upper.st>
2025-04-21 19:47:04 +0200

    bump all manifest files

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -23,15 +23,18 @@ NEW_VERSION="v$MAJOR.$MINOR.$PATCH"
 
 echo "Updating version to $NEW_VERSION"
 
-# Update manifest.json
-jq --arg new_version "$MAJOR.$MINOR.$PATCH" '.version = $new_version' manifest.json > tmp.json && mv tmp.json manifest.json
+# Update manifest.chrome.json (Chromium)
+jq --arg new_version "$MAJOR.$MINOR.$PATCH" '.version = $new_version' manifest.chrome.json > tmp.json && mv tmp.json manifest.chrome.json
+
+# Update manifest.firefox.json
+jq --arg new_version "$MAJOR.$MINOR.$PATCH" '.version = $new_version' manifest.firefox.json > tmp.json && mv tmp.json manifest.firefox.json
 
 # Update package.json
 jq --arg new_version "$MAJOR.$MINOR.$PATCH" '.version = $new_version' package.json > tmp.json && mv tmp.json package.json
 
 npm i --package-lock-only
 
-git add manifest.json package.json package-lock.json 2>/dev/null
+git add manifest.chrome.json manifest.firefox.json package.json package-lock.json 2>/dev/null
 git commit -m "Release $NEW_VERSION"
 git tag "$NEW_VERSION"
 
